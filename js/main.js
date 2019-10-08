6f4fd8292c392efb1c54a9816cf0c12887d67c51
HanHyunJin-mis <forinpea121@gmail.com>
2019-10-08 11:55:29 +0900

    Site updated: 2019-10-08 11:55:28

--- js/main.js
+++ js/main.js
@@ -1,117 +1,96 @@
-(function () {
-  var searchEl = document.querySelector('.search-do'),
-      searchWrap = document.querySelector('.search-wrap'),
-      searchContent = document.querySelector('.search-content'),
-      inputEl = searchWrap.querySelector('input'),
-      resultEl = searchWrap.querySelector('.search-result'),
-      oldResultElString = resultEl.innerHTML,
-      postData;
-  
-  function showSearch (e) {
-    e.preventDefault();
-    searchWrap.style.display = 'flex';
-    inputEl.focus();
-    getSearchData();
-  }
-
-  function hideSearch () {
-    searchWrap.style.display = 'none';
-    resultEl.innerHTML = oldResultElString;
-    postData = null;
-  }
-
-  function indexOfIgnoreTransform (a, b) {
-    a = (a || '').toLocaleLowerCase();
-    b = (b || '').toLocaleLowerCase();
-
-    return a.indexOf(b) > -1;
-  }
-
-  function searchFromJson(data, key) {
-    if (!data || data.length < 1 || !key) {
-      return;
-    }
-    return data.filter(function(post) {
-      if (indexOfIgnoreTransform(post.title, key)) {
-        return true;
-      }
-      if (indexOfIgnoreTransform(post.content, key)) {
-        return true;
-      }
-    })
-  }
-
-  function render(data) {
-    if (!data || data.length < 1) {
-      return resultEl.innerHTML = '<div class="search-no">NOT FIND!</div>';
-    }
-    var htmlString = '',
-      tempEl = document.createElement('div');
-
-    data.forEach(function(post) {
-      tempEl.innerHTML = (post.content || '').slice(0, 100)
-      htmlString += '<li data-url="' + post.url + '"' + '><h3>' + 
-        post.title + '</h3><p>' + tempEl.innerText + '</p></li>'
+(function ($) {
+    $('.article img:not(".not-gallery-item")').each(function () {
+        // wrap images with link and add caption if possible
+        if ($(this).parent('a').length === 0) {
+            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            if (this.alt) {
+                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
+            }
+        }
     });
 
-    resultEl.innerHTML = htmlString;
-  }
-
-  function getSearchData (callback) {
-    if (postData) {
-      callback && callback(postData);
-      return;
+    if (typeof (moment) === 'function') {
+        $('.article-meta time').each(function () {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
     }
-    fetch('/search.json').then(function (respons) {
-      respons.json().then(function(data){
-        postData = data;
-        callback && callback(postData);
-      }); 
-    }).catch(function() {
-      callback && (resultEl.innerHTML = 'An error has occurred！');
+
+    $('.article > .content > table').each(function () {
+        if ($(this).width() > $(this).parent().width()) {
+            $(this).wrap('<div class="table-overflow"></div>');
+        }
     });
-  }
 
-  function doSearch (e) {
-    if (e.isComposing) {
-      return;
+    function adjustNavbar() {
+        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
+        if ($(document).outerWidth() < navbarWidth) {
+            $('.navbar-main .navbar-menu').addClass('is-flex-start');
+        } else {
+            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
+        }
     }
-    if (!e.target.value) {
-      resultEl.innerHTML = oldResultElString;
-      return;
+    adjustNavbar();
+    $(window).resize(adjustNavbar);
+
+    $('figure.highlight table').wrap('<div class="highlight-body">');
+    if (typeof (IcarusThemeSettings) !== 'undefined' &&
+        typeof (IcarusThemeSettings.article) !== 'undefined' &&
+        typeof (IcarusThemeSettings.article.highlight) !== 'undefined') {
+        if (typeof (ClipboardJS) !== 'undefined' && IcarusThemeSettings.article.highlight.clipboard) {
+            $('figure.highlight').each(function () {
+                var id = 'code-' + Date.now() + (Math.random() * 1000 | 0);
+                var button = '<a href="javascript:;" class="copy" title="Copy" data-clipboard-target="#' + id + ' .code"><i class="fas fa-copy"></i></a>';
+                $(this).attr('id', id);
+                if ($(this).find('figcaption').length) {
+                    $(this).find('figcaption').prepend(button);
+                } else {
+                    $(this).prepend('<figcaption>' + button + '</figcaption>');
+                }
+            });
+            new ClipboardJS('.highlight .copy');
+        }
+        var fold = IcarusThemeSettings.article.highlight.fold;
+        if (fold.trim()) {
+            var button = '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
+            $('figure.highlight').each(function () {
+                if ($(this).find('figcaption').length) {
+                    $(this).find('figcaption').prepend(button);
+                } else {
+                    $(this).prepend('<figcaption>' + button + '</figcaption>');
+                }
+            });
+
+            function toggleFold(codeBlock, isFolded) {
+                var $toggle = $(codeBlock).find('.fold i');
+                !isFolded ? $(codeBlock).removeClass('folded') : $(codeBlock).addClass('folded');
+                !isFolded ? $toggle.removeClass('fa-angle-right') : $toggle.removeClass('fa-angle-down');
+                !isFolded ? $toggle.addClass('fa-angle-down') : $toggle.addClass('fa-angle-right');
+            }
+
+            $('figure.highlight').each(function () {
+                toggleFold(this, fold === 'folded');
+            });
+            $('figure.highlight figcaption .fold').click(function () {
+                var $code = $(this).closest('figure.highlight');
+                toggleFold($code.eq(0), !$code.hasClass('folded'));
+            });
+        }
     }
-    getSearchData(function(data){
-      render(searchFromJson(data, e.target.value));
-    });
-  }
 
-  function goto (url) {
-    window.location.href = url;
-  }
+    var $toc = $('#toc');
+    if ($toc.length > 0) {
+        var $mask = $('<div>');
+        $mask.attr('id', 'toc-mask');
 
-  function gotoSearchResult(e) {
-    e.stopPropagation();
-    var el = e.target;
+        $('body').append($mask);
 
-    if (el.tagName !== 'LI') {
-      el = el.parentNode;
+        function toggleToc() {
+            $toc.toggleClass('is-active');
+            $mask.toggleClass('is-active');
+        }
+
+        $toc.on('click', toggleToc);
+        $mask.on('click', toggleToc);
+        $('.navbar-main .catalogue').on('click', toggleToc);
     }
-    if (el.tagName !== 'LI' || !el.dataset.url) {
-      return;
-    }
-    goto(window.location.origin + el.dataset.url);
-  }
-
-  function onDocKeydown (e) {
-    if (e.keyCode === 27) {
-      hideSearch();
-    }
-  }
-
-  searchEl.addEventListener('click', showSearch);
-  inputEl.addEventListener('input', doSearch);
-  searchWrap.addEventListener('click', hideSearch);
-  searchContent.addEventListener('click', gotoSearchResult);
-  inputEl.addEventListener('compositionend', doSearch);
-  document.addEventListener('keydown', onDocKeydown);
-})();
+})(jQuery);
